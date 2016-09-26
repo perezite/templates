16d08fdea2157b62e31c1cd1e20d6466c32e16a8
Simon Schwegler <simulo2000@gmail.com>
2016-09-27 00:07:51 +0200

    Cleanup

--- SDL2Android/jni/src/Android.mk
+++ SDL2Android/jni/src/Android.mk
@@ -10,7 +10,10 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-	main.cpp
+    main.cpp
+# LOCAL_SRC_FILES := main.cpp
+
+# LOCAL_SRC_FILES := main.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
