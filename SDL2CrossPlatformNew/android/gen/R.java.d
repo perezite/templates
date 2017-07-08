a7aa82ee74af9cada20ba11eae7b7d0233c41036
Simon Schwegler <simulo2000@gmail.com>
2017-07-08 22:49:19 +0200

    Added basic project

---
+++ SDL2CrossPlatformNew/android/gen/R.java.d
@@ -0,0 +1,8 @@
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\gen\org\libsdl\app\R.java \
+ : C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\drawable-hdpi\ic_launcher.png \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\drawable-mdpi\ic_launcher.png \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\drawable-xxhdpi\ic_launcher.png \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\layout\main.xml \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\res\values\strings.xml \
+C:\Users\simon_000\Desktop\VC_SDL2_Android_SetupTest\project\android\bin\AndroidManifest.xml \
