3a6dfb1f07bcb131c165dc59ff0835808c8fcb07
Simon Schwegler <simulo2000@gmail.com>
2019-01-19 13:45:41 +0100

    Added gitignore

--- SDL2CrossPlatform/build/External/Android/lib-build/SDL2-2.0.5/jni/Application.mk
+++ SDL2CrossPlatform/build/External/Android/lib-build/SDL2-2.0.5/jni/Application.mk
@@ -5,8 +5,9 @@ APP_STL := stlport_static
 
 # APP_ABI := armeabi armeabi-v7a x86
 # APP_ABI := armeabi-v7a
+# APP_ABI := x86
 APP_ABI := armeabi-v7a x86
 
-# Min SDK level
+# Should be set equal to the Android Mininum SDK level in AndroidManifest.xml, see https://stackoverflow.com/questions/21888052/what-is-the-relation-between-app-platform-androidminsdkversion-and-androidtar
 APP_PLATFORM=android-19
 
